d468f8e8217b75784a8c714c510bd24cb661d37f
Sergen Keskincelik <skeskinc@mail.uni-mannheim.de>
2021-03-20 15:18:38 +0100

    #10 Created player model classes

---
+++ src/scrabble/model/HumanPlayer.java
@@ -0,0 +1,48 @@
+package model;
+
+public class HumanPlayer extends Player {
+	
+	//Amount of games won for statistics -> HumanPlayer
+	private int gamesWon;
+	//Amount of games lost for statistics -> HumanPlayer
+	private int gamesLost;
+	//Your average Win-Rate
+	private double winrate;
+	
+	public HumanPlayer(String name) {
+		super(name);
+	}
+	
+	public void setWinRate(double winrate) {
+		this.winrate = winrate;
+	}
+	
+	public double getWinRate() {
+		return this.winrate;
+	}
+	
+	public int getGamesLost() {
+		return gamesLost;
+	}
+
+	public void setGamesLost(int gamesLost) {
+		this.gamesLost = gamesLost;
+	}
+	
+	private int getGamesWon() {
+		return this.gamesWon;
+	}
+	public void gamesWon(int gamesWon) {
+		this.gamesWon = gamesWon;
+	}
+
+	/*
+	public static void main(String[] args) {
+		HumanPlayer hp = new HumanPlayer("Scrabble Player 1");
+		hp.setScore(2);
+		System.out.println("Current Player: " + hp.getName());
+		System.out.println("Current Score: " + hp.getScore());
+	}
+	*/
+
+}
---
+++ src/scrabble/model/Player.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.Date;
+
+public abstract class Player {
+	
+	//Name of the Player
+	private String name;
+	//Amount of points which a player has received in the game
+	private int score;
+	//Rack to place your tiles
+	private String[] rack = new String[7];
+	//Turn options for each player
+	private String[] turnOptions = {"pass","exchange","play"};
+	//Necessary to determine, if one player used more than 10 minutes of overtime
+	private Date usedOvertime;
+	
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	//Setting your Rack in the beginning or while choosing the options 'exchange' or 'play'
+	public void settingRack() {
+		//Code needs to be implemented.
+	}
+	//Clearing the rack, e.g. in the end of the game
+	public void clearRack() {
+		rack = new String[7];
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
