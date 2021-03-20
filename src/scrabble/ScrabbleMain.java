830ad04bda5388835d6907a3f2954faa752c3324
Eldar Kasmamytov <ekasmamy@mail.uni-mannheim.de>
2021-03-20 13:57:08 +0100

    #6 Basic Directories Structure
    - Added ScrabbleMain class (package: scrabble). Runs without any issues.
    - Added an application icon
    - Changes to .gitignore file

---
+++ src/scrabble/ScrabbleMain.java
@@ -0,0 +1,32 @@
+package scrabble;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class ScrabbleMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("fxml/interface.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1080, 720);
+        scene.getStylesheets().add(ScrabbleMain.class.getResource("css/style.css").toExternalForm());
+
+        Image appIcon = new Image(ScrabbleMain.class.getResourceAsStream("img/scrabble-icon-dark.png"));
+
+        primaryStage.setTitle("Scrabble");
+        primaryStage.getIcons().add(appIcon);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
