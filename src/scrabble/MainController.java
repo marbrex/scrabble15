8237c745368293146646809b48b4ccd7f2511c51
Eldar Kasmamytov <ekasmamy@mail.uni-mannheim.de>
2021-03-22 00:44:13 +0100

    #6 Interface Improvements
    - Added a rectangle in each cell of the game grid
    - Started working on Drag&Drop for Letter Blocks (not working yet)

--- src/scrabble/MainController.java
+++ src/scrabble/MainController.java
@@ -1,15 +1,26 @@
 package scrabble;
 
 import com.jfoenix.controls.JFXBadge;
+import com.jfoenix.controls.JFXButton;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -22,8 +33,18 @@ public class MainController {
     // the object reference from FXML is automatically copied into the controller instance variable.
 
     @FXML
-    // List of currently proposed Letters
-    private List<Button> letters;
+    private HBox lettersBlock;
+
+//    @FXML
+//    // List of currently proposed Letters
+//    private ArrayList<JFXButton> letters;
+
+    @FXML
+    // Grid of letters
+    private GridPane grid;
+
+    @FXML
+    private JFXButton okBtn;
 
     @FXML
     // The location of the FXML document
@@ -35,14 +56,85 @@ public class MainController {
     // Default constructor
     public MainController() {}
 
+    private int gridSize = 825;
+    private int cellNumber = 15;
+    private int gridPadSize = 5;
+    private double cellSize = gridSize/cellNumber - gridPadSize;
+
+    private int lettersNumber = 7;
+
     @FXML
     // The FXML loader will call the initialize() method after the loading of the FXML document is complete
     private void initialize () {
 
-//        for (int i=0; i<letters.size(); i++) {
-//            Button ltr = letters.get(i);
-//
-//        }
+        grid.setPadding(new Insets(gridPadSize));
+        grid.setHgap(gridPadSize);
+        grid.setVgap(gridPadSize);
 
+        for (int i=0; i<cellNumber; i++) {
+            for (int j=0; j<cellNumber; j++) {
+                Rectangle rect = new Rectangle(i, j, cellSize, cellSize);
+                rect.setFill(Color.AQUA);
+                rect.setStroke(Color.DARKGRAY);
+                rect.setArcWidth(10);
+                rect.setArcHeight(10);
+
+                InnerShadow is = new InnerShadow();
+                is.setColor(Color.DARKGRAY);
+                is.setOffsetX(-5);
+                is.setOffsetY(5);
+                is.setHeight(20);
+                is.setWidth(20);
+
+                rect.setEffect(is);
+
+                rect.setOnMouseEntered(event -> rect.setFill(Color.CRIMSON));
+                rect.setOnMouseExited(event -> rect.setFill(Color.AQUA));
+
+                GridPane.setConstraints(rect, i, j);
+
+                grid.getChildren().add(rect);
+            }
+        }
+
+        for (int i=0; i<lettersNumber; i++) {
+            Button ltr = new Button("S");
+            ltr.getStyleClass().add("letter-btn");
+            ltr.setPrefSize(cellSize, cellSize);
+
+            DropShadow ds = new DropShadow();
+            ds.setHeight(20);
+            ds.setWidth(20);
+            ds.setOffsetY(-3);
+            ds.setOffsetX(3);
+            ds.setColor(Color.GRAY);
+
+            ltr.setEffect(ds);
+
+            ltr.setOnMousePressed(event -> {
+                ltr.setStyle("-fx-background-color: GREEN");
+                System.out.println("Event.getScene: (" + event.getSceneX() + ", " + event.getSceneY() + ")");
+                System.out.println("Event.get: (" + event.getX() + ", " + event.getY() + ")\n");
+            });
+
+            ltr.setOnMouseDragged(event -> {
+                Bounds boundsInScene = ltr.localToScene(ltr.getBoundsInLocal());
+                ltr.setTranslateX(event.getSceneX() - boundsInScene.getMinX());
+                ltr.setTranslateY(event.getSceneY() - boundsInScene.getMinY());
+                System.out.println("Translate: (" + ltr.getTranslateX() + ", " + ltr.getTranslateY() + ")");
+                System.out.println("BoundsInScene: (" + boundsInScene.getMinX() + ", " + boundsInScene.getMinY() + ")\n");
+                ltr.setCursor(Cursor.CLOSED_HAND);
+            });
+
+            ltr.setOnMouseReleased(event -> {
+                ltr.setCursor(Cursor.DEFAULT);
+                ltr.setStyle("-fx-background-color: ORANGE");
+            });
+
+            ltr.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> ltr.setStyle("-fx-opacity: 0.8"));
+            ltr.addEventHandler(MouseEvent.MOUSE_EXITED, event -> ltr.setStyle("-fx-opacity: 1"));
+
+            lettersBlock.getChildren().add(ltr);
+        }
     }
 }
