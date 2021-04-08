775d44c3bfcc4a7d0fca69619826b29b30e87eda
Alexander Starchenkov <astarche@mail.uni-mannheim.de>
2021-04-08 12:52:20 +0300

    #10 Updating AiPlayer class

--- src/scrabble/model/AiPlayer.java
+++ src/scrabble/model/AiPlayer.java
@@ -1,5 +1,8 @@
 package scrabble.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AiPlayer extends Player {
 	
 	/**
@@ -35,5 +38,75 @@ public class AiPlayer extends Player {
 	/** Returns the difficulty of the AI-Player */
 	public String getDifficulty() {
 		return this.chosenDifficulty;
+	}
+	/**
+	 * Checks if the word can be made with the available letters
+	 *
+	 * @author Alexander Starchenkov
+	 */
+	public boolean findWord(List<Letter> letters, String toFind) {
+		boolean found = false;
+		if (this.hasLettersOf(this.getValues(letters), toFind)) {
+			String hl = clear(getValues(letters).toString(), toFind);
+			if (hl.length() >= toFind.length()) {
+				found = true;
+			}
+		}
+		return found;
+	}
+	/**
+	 * Returns a list with all possible words from the dictionary that can be made with the given letters
+	 *
+	 * @author Alexander Starchenkov
+	 */
+	public List<String> findWords(List<Letter> letters, List<String> words){
+		List<String> foundWords = new ArrayList<String>();
+		for (int i = 0;i < words.size();i++){
+			if(this.findWord(letters, words.get(i))){
+				foundWords.add(words.get(i));
+			}
+		}
+		return foundWords;
+	}
+	/**
+	 * Checks if all letters to make the searched word are present
+	 *
+	 * @author Alexander Starchenkov
+	 */
+	public boolean hasLettersOf(List<Character> currentLetters, String word){
+		boolean res = true;
+		for (int i = 0;i < word.length();i++){
+			if (!currentLetters.contains(word.charAt(i))){
+				res = false;
+				break;
+			}
+		}
+		return res;
+	}
+	/**
+	 * Returns a list with char values of the letters
+	 *
+	 * @author Alexander Starchenkov
+	 */
+	public List<Character> getValues(List<Letter> letters){
+		List<Character> values = new ArrayList<Character>();
+		for (int i = 0;i < letters.size();i++) {
+			values.add(letters.get(i).getLetter());
+		}
+		return values;
+	}
+	/**
+	 * Removes all the letters that are not used in the searched word
+	 *
+	 * @author Alexander Starchenkov
+	 */
+	public String clear(String currentLetters, String word){
+		String cleared = "";
+		for(int i = 0;i < currentLetters.length();i++){
+			if (word.contains(currentLetters.charAt(i) +"")){
+				cleared += currentLetters.charAt(i);
+			}
+		}
+		return cleared;
 	}
 }
