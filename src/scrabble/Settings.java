2f5f5c2072c604f54cbf9a179c114d9df8a00384
Alexander Starchenkov <astarche@mail.uni-mannheim.de>
2021-04-08 16:40:35 +0300

    #6 settings window

---
+++ src/scrabble/Settings.java
@@ -0,0 +1,30 @@
+package scrabble;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Settings extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("fxml/Settings.fxml"));
+            Pane root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
