64e3aa4d3bbfcee6dfbd71234349e77d13aaf413
Alexander Starchenkov <astarche@mail.uni-mannheim.de>
2021-04-13 18:51:42 +0300

    #6 Updating settings

--- src/scrabble/Settings.java
+++ src/scrabble/Settings.java
@@ -16,7 +16,7 @@ public class Settings extends Application {
     public void start(Stage stage) throws Exception {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("fxml/SettingsVideo.fxml"));
+            loader.setLocation(getClass().getResource("fxml/Settings.fxml"));
             Pane root = loader.load();
             Scene scene = new Scene(root);
             stage.setScene(scene);
