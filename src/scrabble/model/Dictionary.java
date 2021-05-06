72e56bf3e6c295efd1e023ff5324bbcbd00362a1
Sergen Keskincelik <skeskinc@mail.uni-mannheim.de>
2021-05-06 14:32:35 +0200

    #20 Improving Java-Doc Comments of Player models and Dictionary

--- src/scrabble/model/Dictionary.java
+++ src/scrabble/model/Dictionary.java
@@ -8,19 +8,21 @@ import scrabble.game.Word;
  * scrabble.model.Dictionary class to import the word list, adding or removing words from the
  * Dictionary
  * 
- * @author Sergen Keskincelik
+ * @author skeskinc
  */
 
 public class Dictionary {
 
-  /** BufferedReader to read each line of the Dictionary */
   private static BufferedReader in;
-  /** ArrayList to store all the words in the Dictionary */
   private static List<String> words = new ArrayList<String>();
-  /** ArrayList to store all the definitions in the Dictionary */
   private static List<String> definitions = new ArrayList<String>();
 
-  /** Setting the Dictionary -> following the Format is necessary! */
+  /**
+   * Setting the Dictionary -> following the Format is necessary!
+   * 
+   * @param file to read the File
+   * @author skeskinc
+   */
   public static void setDictionary(InputStream file) {
     try {
       in = new BufferedReader(new InputStreamReader(file));
@@ -59,25 +61,44 @@ public class Dictionary {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-
   }
 
-  /** Return the list of words */
+  /**
+   * Returns a list of words
+   * 
+   * @return list of words
+   * @author skeskinc
+   */
   public static List<String> getWords() {
     return words;
   }
 
-  /** Returns the list of all Definitions */
+  /**
+   * Returns a list of all Definitions
+   * 
+   * @return all definitions which are present in the File
+   * @author skeskinc
+   */
   public static List<String> getDefinitions() {
     return definitions;
   }
 
-  /** Returns the size of the Dictionary */
+  /**
+   * Returns the size of the Dictionary
+   * 
+   * @return Size of the Dictionary
+   * @author skeskinc
+   */
   public static int getDictionarySize() {
     return getWords().size();
   }
 
-  /** Checks, if given word matches a word of the Dictionary */
+  /**
+   * Checks, if given word matches a word of the Dictionary
+   * 
+   * @param word Matching given word with words from Dictionary
+   * @author skeskinc
+   */
   public static boolean matches(Word word) {
     for (String dictionaryWord : words) {
       if (word.getWordAsString().equals(dictionaryWord)) {
@@ -87,7 +108,12 @@ public class Dictionary {
     return false;
   }
 
-  /** Adds a Word into the Word-List */
+  /**
+   * Adds a Word into the Word-List
+   * 
+   * @param word Adding given word to dictionary
+   * @author skeskinc
+   */
   public static void addWord(Word word) {
     if (!matches(word)) {
       words.add(word.getWordAsString());
@@ -95,7 +121,12 @@ public class Dictionary {
     }
   }
 
-  /** Removes Word from the Word-List */
+  /**
+   * Removes Word from the Word-List
+   * 
+   * @param word Removing given word from dictionary
+   * @author skeskinc
+   */
   public static void removeWord(Word word) {
     for (int i = 0; i < words.size(); i++) {
       if (words.get(i).equals(word.getWordAsString())) {
--- src/scrabble/model/HumanPlayer.java
+++ src/scrabble/model/HumanPlayer.java
@@ -6,26 +6,32 @@ import java.io.Serializable;
  * scrabble.model.HumanPlayer class extends from the scrabble.model.Player class and contains game
  * statistics, which is only necessary for the HumanPlayer
  * 
- * @author Sergen Keskincelik
+ * @author skeskinc
  */
 
 public class HumanPlayer extends Player implements Serializable {
 
-  /** Amount of games won for statistics -> HumanPlayer */
   private int gamesWon;
-  /** Amount of games lost for statistics -> HumanPlayer */
   private int gamesLost;
-  /** Determine, whether a human player is a host or not */
   private boolean host;
 
-  /** Constructor of the Humanplayer class */
+  /**
+   * Constructor of the Humanplayer class
+   * 
+   * @author skeskinc
+   */
   public HumanPlayer() {
     super();
     this.gamesWon = 0;
     this.gamesLost = 0;
   }
 
-  /** Returns Win-Rate of the player */
+  /**
+   * Returns Win-Rate of the player
+   * 
+   * @return The Win-Rate of the Player
+   * @author skeskinc
+   */
   public double getWinRate() {
     if (gamesLost != 0) {
       return Math.round((gamesWon * 100.0) / gamesLost) / 100.0;
@@ -34,32 +40,62 @@ public class HumanPlayer extends Player implements Serializable {
     }
   }
 
-  /** Returns the amount of games lost in total */
+  /**
+   * Returns the amount of games lost in total
+   * 
+   * @return Amount of games lost
+   * @author skeskinc
+   */
   public int getGamesLost() {
     return gamesLost;
   }
 
-  /** Setting the amount of games lost in total -> Database */
+  /**
+   * Setting the amount of games lost in total -> Database
+   * 
+   * @param gamesLost Setting up the amount of games lost
+   * @author skeskinc
+   */
   public void setGamesLost(int gamesLost) {
     this.gamesLost = gamesLost;
   }
 
-  /** Returns the amount of games won in total */
+  /**
+   * Returns the amount of games won in total
+   * 
+   * @return Amount of games won
+   * @author skeskinc
+   */
   public int getGamesWon() {
     return this.gamesWon;
   }
 
-  /** Setting the amount of games won in total -> Database */
+  /**
+   * Setting the amount of games won in total
+   * 
+   * @param gamesWon Setting up the amount of games won
+   * @author skeskinc
+   */
   public void setGamesWon(int gamesWon) {
     this.gamesWon = gamesWon;
   }
 
-  /** Deciding to make human player a host */
+  /**
+   * Deciding to make human player a host
+   * 
+   * @param host Determine if player is a host or not
+   * @author skeskinc
+   */
   public void setHost(boolean host) {
     this.host = host;
   }
 
-  /** Returns a truth value, if a human player is a host or not */
+  /**
+   * Returns a truth value, if a human player is a host or not
+   * 
+   * @return True if a player is a host, else false
+   * @author skeskinc
+   */
   public boolean isHost() {
     return this.host;
   }
