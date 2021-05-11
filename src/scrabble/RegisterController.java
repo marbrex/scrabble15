108b2534f15d621cbf493181442bf7d432e50c26
Moritz Raucher <mraucher@mail.uni-mannheim.de>
2021-05-11 21:51:47 +0200

    #25 Update and Improvements Player Registration

--- src/scrabble/RegisterController.java
+++ src/scrabble/RegisterController.java
@@ -9,29 +9,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import scrabble.*;
 import scrabble.dbhandler.DBInformation;
 import scrabble.dbhandler.Database;
 import scrabble.model.*;
 
-// @author mraucher
-
+/**
+ * 
+ * The controller class for the Register.fxml screen
+ * 
+ * @author mraucher
+ */
 public class RegisterController implements Initializable {
 
-  String user;
-  @FXML
-  private JFXTextArea txtPlayerId;
-  @FXML
-  private JFXTextArea txtPassword;
+  private static final String nameMissingWarning = "Please choose a name!\n";
+  private static final String nameLengthWarning = "Name should be less than 16 characters!\n";
+  private static final String imageNotSetWarning = "Please choose an image!\n";
+  private static final String nameAlreadyExists =
+      "The name already exists, please choose another name!\n";
+
+  private String user;
   @FXML
   private JFXTextArea txtName;
   @FXML
@@ -39,145 +39,131 @@ public class RegisterController implements Initializable {
   @FXML
   private JFXButton registerBtn;
   @FXML
-  private Pane root;
+  private BorderPane root;
 
   private HumanPlayer plyr;
-  // Database db;
 
+
+  /**
+   * The event handler for the register button
+   * 
+   * @param event ActionEvent if the register button is clicked
+   * @author mraucher
+   */
   @FXML
   public void registerBtnOnAction(ActionEvent event) {
-    /*
-     * try {
-     * 
-     * String playerId = txtPlayerId.getText().trim(); String name = txtName.getText().trim();
-     * String pass = txtPassword.getText().trim();
-     * 
-     * 
-     * plyr.setName(name); // plyr.setUser(user)
-     * 
-     * if (name.isEmpty() || playerId.isEmpty()) {
-     * AlertMessage.showWarningMessage("All input fields are required"); return; }
-     * 
-     * // db.addPlayerProfile(new String[] {playerId, name, pass, user});
-     * 
-     * Database.connectToDB(); if
-     * (!DBInformation.containsIdentification(Integer.parseInt(playerId))) {
-     * Database.fillTables(Integer.parseInt(playerId), name, 1); Database.disconnectDB();
-     * 
-     * showHomePage(); //here problem
-     * 
-     * Stage stage1 = (Stage) txtName.getScene().getWindow(); stage1.close(); } } catch (Exception
-     * e) { e.printStackTrace(); }
-     */ // --> Problems with DB will be implemented later
-    // Debug approach :
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/ChooseProfileScene.fxml"));
-      Parent root = loader.load();
-      Stage stage = (Stage) this.backBtn.getScene().getWindow();
-      Scene scene =
-          new Scene(root, this.root.getScene().getWidth(), this.root.getScene().getHeight());
-      scene.getStylesheets().add(getClass().getResource("css/mainMenu.css").toExternalForm());
-      stage.setScene(scene);
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
 
-  private int generateId() {
-    int id = 0;
-//    Database.connectToDB();
-    for (int i = 1; i < 5; i++) {
-      if (!DBInformation.containsIdentification(i)) {
-        id = i;
-        break;
+    String name = txtName.getText().trim();
+    plyr.setName(name);
+
+    // checking for warnings and construct the warningMessage
+    String warningMessage = "";
+    if (name.isEmpty()) {
+      warningMessage += nameMissingWarning;
+    }
+    if (name.length() > 15) {
+      warningMessage += nameLengthWarning;
+    }
+    if (user == null) {
+      warningMessage += imageNotSetWarning;
+    }
+    if (DBInformation.containsName(name)) {
+      warningMessage += nameAlreadyExists;
+    }
+    if (warningMessage != "") {
+      AlertMessage.showWarningMessage(warningMessage);
+    } else {
+
+      // write new profile to DB
+      Database.fillTables(generateId(), name, user == "male" ? 0
+          : user == "female" ? 1 : user == "anonyms" ? 2 : user == "animal" ? 3 : 0);
+
+      try {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/ChooseProfileScene.fxml"));
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        Scene scene =
+            new Scene(root, this.root.getScene().getWidth(), this.root.getScene().getHeight());
+        scene.getStylesheets().add(getClass().getResource("css/mainMenu.css").toExternalForm());
+        stage.setScene(scene);
+      } catch (IOException e) {
+        e.printStackTrace();
       }
     }
-//    Database.disconnectDB();
-    /*
-     * return (int) Math.floor(Math.random()*(9999-10+1)+10);
-     */
-    return id;
   }
 
-
-  private void showHomePage() throws IOException {
-    FXMLLoader loader = new FXMLLoader();
-    loader.setLocation(getClass().getResource("fxml/ChooseProfileScene.fxml"));
-
-    Stage stage = new Stage();
-    stage.setScene(new Scene(loader.load(), this.root.getWidth(), this.root.getHeight()));
-
-    stage.show();
-  }
-
+  /**
+   * Initializes a new human player
+   * 
+   * @author mraucher
+   */
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
-    txtPlayerId.setEditable(false);
-
     plyr = new HumanPlayer();
-
-    // db = new Database();
-
-    int id = generateId();
-
-    // while(db.isExist(id))
-
-    txtPlayerId.setText(id + "");
-
-    // plyr.setId(id);
-
-    user = "male";
   }
 
-
-
+  /**
+   * Sets the user to female when the corresponding button is pressed
+   * 
+   * @param event ActionEvent if the button is clicked
+   * @author mraucher
+   */
   @FXML
   public void femaleBtnPressed(ActionEvent event) {
 
     user = "female";
   }
 
+  /**
+   * Sets the user to male when the corresponding button is pressed
+   * 
+   * @param event ActionEvent if the button is clicked
+   * @author mraucher
+   */
   @FXML
   public void maleBtnPressed(ActionEvent event) {
 
     user = "male";
   }
 
+  /**
+   * Sets the user to anonymous when the corresponding button is pressed
+   * 
+   * @param event ActionEvent if the button is clicked
+   * @author mraucher
+   */
   @FXML
   public void anonymsBtnPressed(ActionEvent event) {
 
     user = "anonyms";
   }
 
+  /**
+   * Sets the user to animal when the corresponding button is pressed
+   * 
+   * @param event ActionEvent if the button is clicked
+   * @author mraucher
+   */
   @FXML
   public void animalBtnPressed(ActionEvent event) {
 
     user = "animal";
   }
 
-  @FXML
-  public void backBtnOnAction(ActionEvent event) {
-    /*
-     * // Parent root=FXMLLoader.load((getClass().getResource("fxml/signin_signup.fxml")));
-     * BorderPane root = FXMLLoader.load(new URL("file:" + System.getProperty("user.dir") +
-     * "\\resources\\" + "scrabble\\fxml\\ChooseProfileScene.fxml")); Button btn = ((Button)
-     * event.getSource()); Stage stage = (Stage) btn.getScene().getWindow(); //
-     * stage.initStyle(StageStyle.UNDECORATED); stage.setScene(new Scene(root, this.root.getWidth(),
-     * this.root.getHeight()));
-     * 
-     * other approach -> test later
-     * 
-     * try { FXMLLoader loader = new
-     * FXMLLoader(getClass().getResource("fxml/ChooseProfileScene.fxml")); Parent root =
-     * loader.load(); Stage stage = (Stage) this.backBtn.getScene().getWindow(); Scene scene = new
-     * Scene(root, this.root.getScene().getWidth(), this.root.getScene().getHeight());
-     * scene.getStylesheets().add(getClass().getResource("css/mainMenu.css").toExternalForm());
-     * stage.setScene(scene); } catch (IOException e) { // TODO Auto-generated catch block
-     * e.printStackTrace(); }
-     */
-    // --> will be implemented later
-
+  /**
+   * Generating a player id
+   * 
+   * @return the generated Id
+   * @author mraucher
+   */
+  private int generateId() {
+    int id = 0;
+    for (int i = 1; i < 5; i++) {
+      if (!DBInformation.containsIdentification(i)) {
+        id = i;
+        break;
+      }
+    }
+    return id;
   }
 
 }
