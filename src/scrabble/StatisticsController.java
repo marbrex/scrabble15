d80fbdb1091665a2802690ecf839f35c8a7311d4
Moritz Raucher <mraucher@mail.uni-mannheim.de>
2021-05-11 22:03:48 +0200

    #21 Update Statistics Controller

--- src/scrabble/StatisticsController.java
+++ src/scrabble/StatisticsController.java
@@ -1,40 +1,44 @@
 package scrabble;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 
 import scrabble.dbhandler.DBInformation;
 import scrabble.dbhandler.Database;
 import scrabble.model.HumanPlayer;
+import scrabble.model.Profile;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
-// @author mraucher
+/**
+ * 
+ * The controller class for the Statistics.fxml screen
+ * 
+ * @author mraucher
+ */
 
-public class StatisticsController {
+public class StatisticsController implements Initializable {
 
-
-  Database db;
-  DBInformation dbi;
-
-  @FXML
-  private ImageView img;
   @FXML
   private JFXTextArea txtName;
   @FXML
@@ -44,103 +48,104 @@ public class StatisticsController {
   @FXML
   private JFXTextArea txtGamesPlayed;
   @FXML
-  private JFXTextArea txtLastGame;
-  @FXML
   private JFXTextArea txtRatio;
   @FXML
   private PieChart pieChart;
   @FXML
   private JFXButton backBtn;
   @FXML
-  private StackPane root;
+  private BorderPane root;
+  @FXML
+  private ImageView img;
 
   private HumanPlayer plyr;
 
-  public void init(HumanPlayer plyr) throws FileNotFoundException {
-    /*
-     * this.plyr = plyr;
-     * 
-     * db = new Database(); dbi = new DBInformation();
-     * 
-     * List<String> val = dbi.getPlayerStat(plyr.getId());
-     * 
-     * this.txtName.setText(plyr.getName());
-     * this.txtGamesPlayed.setText("How many games have been played? "+val.get(0));
-     * this.txtLastGame.setText("When the last game was played? "+val.get(1));
-     * this.txtWon.setText("Games won: "+plyr.getGamesWon());
-     * this.txtLost.setText("Games lost: "+plyr.getGamesLost());
-     * this.txtRatio.setText("Win/Lose Ratio: "+plyr.getWinRate()+""); this.img.setImage(new
-     * Image(new FileInputStream("resources/images/"+plyr.getUser()+".png")));
-     * //@../../../resources/images/ProfilePicture.png
-     * 
-     * int win = (int) Math.round(plyr.getWinRate()); int lost = 100-win;
-     * 
-     * ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList( new
-     * PieChart.Data("Lost", lost), new PieChart.Data("Win", win));
-     * 
-     * pieChart.setData(pieChartData);
-     */
-    // out commented for compiling purpose --> need for DB update in Future
+  
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    this.plyr = Profile.getPlayer();
 
+    List<String> val = DBInformation.getPlayerStatistic(plyr);
+    int gamesPlayed = Integer.valueOf(val.get(1)) + Integer.valueOf(val.get(2));
+
+    this.txtName.setText(plyr.getName());
+    this.txtGamesPlayed.setText("How many games have been played? " + gamesPlayed);
+    this.txtWon.setText("Games won: " + plyr.getGamesWon());
+    this.txtLost.setText("Games lost: " + plyr.getGamesLost());
+    this.txtRatio.setText("Win/Lose Ratio: " + plyr.getWinRate() + "");
+    String basePath = new File("").getAbsolutePath();
+    System.out.println(basePath);
+    try {
+      this.img
+          .setImage(new Image(new FileInputStream(".\\src\\resources\\scrabble\\img\\male.png")));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    // pie chart with wins vs. losses will only be shown if at least one game was played
+    if (plyr.getGamesLost() + plyr.getGamesWon() > 0) {
+      ObservableList<PieChart.Data> pieChartData =
+          FXCollections.observableArrayList(new PieChart.Data("Lost", plyr.getGamesLost()),
+              new PieChart.Data("Win", plyr.getGamesWon()));
+
+      pieChart.setData(pieChartData);
+    }
   }
 
+  /**
+   * Listener for the update button.
+   * The players name will be updated in the database
+   * 
+   * @param event the ActionEvent if the delete profile button is clicked
+   * @author mraucher
+   */
   @FXML
   public void updateBtnOnAction(ActionEvent event) {
-    /*
-     * String name = txtName.getText();
-     * 
-     * new Database().updatePlayerName(plyr, name);
-     */
-    // out commented for compiling purpose --> need for DB update in Future
+    String newName = txtName.getText();
+    Database.updatePlayerName(plyr, newName);
   }
 
 
-
+  /**
+   * Listener for the delete profile button.
+   * The current player will be deleted from database and the user will be routed
+   * to the register screen.
+   * 
+   * @param event the ActionEvent if the delete profile button is clicked
+   * @author mraucher
+   */
   @FXML
   public void delBtnOnAction(ActionEvent event) {
-    /*
-     * db.deleteProfile(plyr);
-     * 
-     * try {
-     * 
-     * Parent root=FXMLLoader.load((getClass().getResource("fxml/signin_signup.fxml")));
-     * 
-     * Stage stage = new Stage(); stage.initStyle(StageStyle.UNDECORATED); stage.setScene(new
-     * Scene(root, 800, 600)); stage.show();
-     * 
-     * 
-     * Stage stage1 = (Stage) txtName.getScene().getWindow(); stage1.close();
-     * 
-     * } catch (IOException e) { e.printStackTrace(); }
-     */
-    // out commented for compiling purpose --> need for DB update in Future
-  }
-
-  @FXML
-  public void backBtnOnAction(ActionEvent event) {
-    /*
-     * FXMLLoader loader = new FXMLLoader( getClass().getResource( "fxml/home.fxml" ) );
-     * 
-     * Stage stage = new Stage(); try { stage.setScene( new Scene(loader.load(), 800, 600) ); }
-     * catch (IOException e) { e.printStackTrace(); }
-     * 
-     * HomeController controller = loader.getController(); controller.init(plyr);
-     * 
-     * Stage stage1 = (Stage) txtName.getScene().getWindow(); stage1.close();
-     * 
-     * stage.show();
-     */
-    // out commented for compiling purpose --> need for DB update in Future
+    Database.removePlayer(plyr);
     try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/MainPage.fxml"));
-      Parent root = loader.load();
+
+      Parent root = FXMLLoader.load(getClass().getResource("fxml/Register.fxml"));
       Stage stage = (Stage) this.backBtn.getScene().getWindow();
       Scene scene =
           new Scene(root, this.root.getScene().getWidth(), this.root.getScene().getHeight());
       scene.getStylesheets().add(getClass().getResource("css/mainMenu.css").toExternalForm());
       stage.setScene(scene);
     } catch (IOException e) {
-      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Listener for the back button. The user will be routed back to the main page.
+   * 
+   * @param event the ActionEvent if the back button is clicked
+   * @author mraucher
+   */
+  @FXML
+  public void backBtnOnAction(ActionEvent event) {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("fxml/MainPage.fxml"));
+      Stage stage = (Stage) this.backBtn.getScene().getWindow();
+      Scene scene =
+          new Scene(root, this.root.getScene().getWidth(), this.root.getScene().getHeight());
+      scene.getStylesheets().add(getClass().getResource("css/mainMenu.css").toExternalForm());
+      stage.setScene(scene);
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
