452900ec233038c62847f4399959c2450d7b8bc3
Moritz Raucher <mraucher@mail.uni-mannheim.de>
2021-05-27 20:06:24 +0200

    #25 Register Update

--- src/scrabble/RegisterController.java
+++ src/scrabble/RegisterController.java
@@ -79,16 +79,16 @@ public class RegisterController implements Initializable {
           : user == "female" ? 1 : user == "anonyms" ? 2 : user == "animal" ? 3 : 0);
 
       try {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/ChooseProfileScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/ChooseProfileScene.fxml"));
         /*
         Stage stage = (Stage) this.root.getScene().getWindow();
         Scene scene =
             new Scene(root, this.root.getScene().getWidth(), this.root.getScene().getHeight());
-        scene.getStylesheets().add(getClass().getResource("css/changeProfile.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/css/changeProfile.css").toExternalForm());
         stage.setScene(scene);
         */
         ScrabbleApp.getScene().getStylesheets().clear();
-        ScrabbleApp.getScene().getStylesheets().add(getClass().getResource("css/changeProfile.css").toExternalForm());
+        ScrabbleApp.getScene().getStylesheets().add(getClass().getResource("/css/changeProfile.css").toExternalForm());
         ScrabbleApp.getScene().setRoot(root);
       } catch (IOException e) {
         e.printStackTrace();
